4638a357b8cea21e7d27f2c75efdaf85bc3c179e
MD SIFAT KHAN <sifatkhann229@gmail.com>
2022-03-02 18:08:56 +0800

    phone details added

--- js/goribz-phone.js
+++ js/goribz-phone.js
@@ -38,9 +38,41 @@ const loadPhoneDetails = mainFeatures => {
     const url = ` https://openapi.programming-hero.com/api/phone/${mainFeatures}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => displayPhoneDetails(data.data.mainFeatures))
+    .then(data => displayPhoneDetails(data.data))
 }
 
 const displayPhoneDetails = phoneDetail => {
     console.log(phoneDetail);
+    const phoneDetails = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${phoneDetail.image}" class="card-img-top w-50 mx-auto" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${phoneDetail.name}</h5>
+        <p class="card-text">Chipset: ${phoneDetail.mainFeatures.chipSet}</p>
+        <p class="card-text">Display size: ${phoneDetail.mainFeatures.displaySize}</p>
+        <p class="card-text">Memory: ${phoneDetail.mainFeatures.memory}</p>
+        <p class="card-text">Bluetooth: ${phoneDetail.others.Bluetooth}</p>
+        <p class="card-text">GPS: ${phoneDetail.others.GPS}</p>
+        <p class="card-text">NFC: ${phoneDetail.others.NFC}</p>
+        <p class="card-text">Radio: ${phoneDetail.others.Radio}</p>
+        <p class="card-text">USB: ${phoneDetail.others.USB}</p>
+        <p class="card-text">WLAN: ${phoneDetail.others.WLAN}</p>
+
+        // <p class="card-text">
+
+        //     if(phoneDetail.releaseDate === ''){
+        //         <p class="card-text">No release date found</p>
+        //     }
+        //     else{
+        //         <p class="card-text">${phoneDetail.releaseDate}</p>
+        //     }
+        // </p>
+
+        // <button onclick="" type="button" class="btn btn-secondary btn-sm">Show More</button>
+    </div>
+    </div>
+    `;
+    phoneDetails.appendChild(div);
 }
