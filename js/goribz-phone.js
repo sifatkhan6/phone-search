1b036ffa1df380367e3499177cbd8a0b5c580653
MD SIFAT KHAN <sifatkhann229@gmail.com>
2022-03-03 00:45:12 +0800

    final touch

--- js/goribz-phone.js
+++ js/goribz-phone.js
@@ -37,6 +37,7 @@ const searchPhone = () => {
 
                 searchResult.appendChild(div);
 
+                // error message 
                 const noResult = document.getElementById('no-result');
                 noResult.style.display = "none";
             })
@@ -46,7 +47,7 @@ const searchPhone = () => {
 }
 
 const loadPhoneDetails = mainFeatures => {
-    console.log(mainFeatures);
+    // console.log(mainFeatures);
     const url = ` https://openapi.programming-hero.com/api/phone/${mainFeatures}`;
     fetch(url)
     .then(res => res.json())
@@ -54,38 +55,39 @@ const loadPhoneDetails = mainFeatures => {
 }
 
 const displayPhoneDetails = phoneDetail => {
-    console.log(phoneDetail);
+    // console.log(phoneDetail);
     const phoneDetails = document.getElementById('phone-details');
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
     <img src="${phoneDetail.image}" class="card-img-top w-50 mx-auto" alt="...">
     <div class="card-body">
-        <h5 class="card-title">${phoneDetail.name}</h5>
+        <h4 class="card-title">${phoneDetail.name}</h4>
+
+        <h5 class="card-text">Main Features</h5>
         <p class="card-text">Chipset: ${phoneDetail.mainFeatures.chipSet}</p>
         <p class="card-text">Display size: ${phoneDetail.mainFeatures.displaySize}</p>
         <p class="card-text">Memory: ${phoneDetail.mainFeatures.memory}</p>
-        <p class="card-text">Bluetooth: ${phoneDetail.others.Bluetooth}</p>
-        <p class="card-text">GPS: ${phoneDetail.others.GPS}</p>
-        <p class="card-text">NFC: ${phoneDetail.others.NFC}</p>
-        <p class="card-text">Radio: ${phoneDetail.others.Radio}</p>
-        <p class="card-text">USB: ${phoneDetail.others.USB}</p>
-        <p class="card-text">WLAN: ${phoneDetail.others.WLAN}</p>
-        <p class="card-text">${phoneDetail.releaseDate}</p>
+        
+        <h5 class="card-text">Sensors</h5>
+        <p class="card-text">Yes: ${phoneDetail.mainFeatures.sensors[0]}</p>
+        <p class="card-text">Yes: ${phoneDetail.mainFeatures.sensors[1]}</p>
+        <p class="card-text">Yes: ${phoneDetail.mainFeatures.sensors[2]}</p>
+        <p class="card-text">Yes: ${phoneDetail.mainFeatures.sensors[3]}</p>
+        <p class="card-text">Yes: ${phoneDetail.mainFeatures.sensors[4]}</p>
+        <p class="card-text">Yes: ${phoneDetail.mainFeatures.sensors[5]}</p>
+
+        <h5 class="card-text">Others</h5>
+        <p class="card-text">Bluetooth: ${phoneDetail.others?.Bluetooth}</p>
+        <p class="card-text">GPS: ${phoneDetail.others?.GPS}</p>
+        <p class="card-text">NFC: ${phoneDetail.others?.NFC}</p>
+        <p class="card-text">Radio: ${phoneDetail.others?.Radio}</p>
+        <p class="card-text">USB: ${phoneDetail.others?.USB}</p>
+        <p class="card-text">${phoneDetail?.releaseDate}</p>
     </div>
     </div>
     `;
+
     phoneDetails.appendChild(div);
+    
 }
-
-// <p class="card-text">
-
-        //     if(phoneDetail.releaseDate === ''){
-        //         <p class="card-text">No release date found</p>
-        //     }
-        //     else{
-        //         <p class="card-text">${phoneDetail.releaseDate}</p>
-        //     }
-        // </p>
-
-        // <button onclick="" type="button" class="btn btn-secondary btn-sm">Show More</button>
